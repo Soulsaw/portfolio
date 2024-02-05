2a901de2676de7cc4b6d8a8cf90090ccd6e2f4b4
Soulsaw <ssawadogo292@gmail.com>
2024-02-05 17:37:20 +0000

    few

--- src/main.c
+++ src/main.c
@@ -23,7 +23,9 @@ int main(int argc, char *argv[])
     SDL_Renderer *renderer = initializeSDL();
 
     if (renderer == NULL)
+    {
         return 1;
+    }
 
     /* Load wall and player textures */
     SDL_Texture *wallTexture = loadTexture(renderer, "assets/tile2.png");
